fe65b40d7337f97b31cdcd21a3d81b5d00c693cf
ihorsniezhko <ihor.sniezhko@gmail.com>
2025-02-27 20:43:54 +0100

    Display Division  question to the user fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,7 +44,7 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract" ) {
         displaySubtractQuestion(num1, num2);
-      else if (gameType === "division")  {
+    }  else if (gameType === "division")  {
         displayDivisionQuestion(num1, num2);
     }  else {
         alert(`Unknown game type: ${gameType}`);
